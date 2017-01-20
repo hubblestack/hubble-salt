3d9d605a8b20fdbfb6bd93950d185b3536853ba6
Colton Myers <colton.myers@gmail.com>
2017-01-20 14:04:56 -0700

    Rev to v2017.1.1

--- _beacons/win_pulsar.py
+++ _beacons/win_pulsar.py
@@ -25,7 +25,7 @@ DEFAULT_MASK = ['ExecuteFile', 'Write', 'Delete', 'DeleteSubdirectoriesAndFiles'
 DEFAULT_TYPE = 'all'
 
 __virtualname__ = 'pulsar'
-__version__ = 'v2017.1.0'
+__version__ = 'v2017.1.1'
 CONFIG = None
 CONFIG_STALENESS = 0
 
