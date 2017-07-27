e71e618b7d07ef5512f2c29e15ac137f9f4304e6
Colton Myers <colton.myers@gmail.com>
2017-07-27 13:27:17 -0600

    Finish fixing hubble_dir

--- _modules/nova_loader.py
+++ _modules/nova_loader.py
@@ -1758,7 +1758,7 @@ class NovaLazyLoader(LazyLoader):
                         # Python only in the modules directory, yaml only
                         # in the profiles directory. This is hacky but was a
                         # quick fix.
-                        nova_module_cache, nova_profile_cache = hubble_dir
+                        nova_module_cache, nova_profile_cache = self.hubble_dir
                         nova_module_cache = os.path.join(nova_module_cache, '')
                         nova_profile_cache = os.path.join(nova_profile_cache, '')
                         if ext == '.py' and fpath.startswith(nova_profile_cache):
